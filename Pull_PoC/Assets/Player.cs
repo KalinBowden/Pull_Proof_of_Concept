3b2ca03ff67a009b1ee06f61a531b6cfc04be34f
Kalin Bowden <kalinbowden@gmail.com>
2018-12-28 16:10:28 -0600

    Added basic scene

---
+++ Pull_PoC/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Player Variales
+    public 
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
